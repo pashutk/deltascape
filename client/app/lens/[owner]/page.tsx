5acad88ca3beb48eab07813201b7dfd2a98a07db
pashutk <me@pashutk.ru>
2023-05-07 12:17:14 +0200

    List orgs and repos

--- client/app/lens/[owner]/page.tsx
+++ client/app/lens/[owner]/page.tsx
@@ -1,3 +1,84 @@
-export default function Owner() {
-  return <div>Owner</div>;
+import { formatRelative } from "date-fns";
+import { format, parseISO } from "date-fns";
+import Link from "next/link";
+import { Octokit } from "octokit";
+
+async function fetchRepos(owner: string) {
+  const octokit = new Octokit({
+    auth: process.env.OCTOKIT_API_KEY,
+  });
+  if (owner === "Effect-TS") {
+    const { data } = await octokit.rest.repos.get({ owner, repo: "Effect-TS" });
+    return [
+      {
+        id: "schema",
+        description: data.description,
+        updatedAt: parseISO(data.updated_at),
+      },
+    ];
+  }
+  return [];
+}
+
+export default async function Owner({
+  params: { owner },
+}: {
+  params: { owner: string };
+}) {
+  const repos = await fetchRepos(owner);
+  console.log(repos);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="relative flex flex-col place-items-center">
+        <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white mb-16">
+          Select repo in {owner}
+        </h1>
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-w-4xl">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-12 py-3">
+                  Name
+                </th>
+                <th scope="col" className="px-12 py-3">
+                  Description
+                </th>
+                <th scope="col" className="px-12 py-3">
+                  Pull requests
+                </th>
+                <th scope="col" className="px-12 py-3">
+                  Last update
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {repos.map(({ id, description, updatedAt }) => (
+                <tr
+                  key={id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                >
+                  <td
+                    scope="row"
+                    className="px-12 py-8 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    <Link
+                      href={`/lens/${owner}/${id}`}
+                      className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      {id}
+                    </Link>
+                  </td>
+                  <td className="px-12 py-8">{description}</td>
+                  <td className="px-12 py-8">Not available</td>
+                  <td className="px-12 py-8">
+                    {formatRelative(updatedAt, new Date())}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </main>
+  );
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
 import Link from "next/link";
+import { Octokit } from "octokit";
 
 async function fetchOwners() {
+  const octokit = new Octokit({ auth: process.env.OCTOKIT_API_KEY });
+  const { data } = await octokit.rest.orgs.get({ org: "Effect-TS" });
   return [
     {
       id: "Effect-TS",
       reposCount: 1,
+      avatarUrl: data.avatar_url,
+      description: data.description,
     },
   ];
 }
@@ -17,30 +23,44 @@ export default async function Home() {
         <h1 className="text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white mb-16">
           Select owner/organization
         </h1>
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-w-4xl">
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                <th scope="col" className="px-12 py-3">
+                <th scope="col" className="px-4 md:px-12 py-2 md:py-3">
+                  Logo
+                </th>
+                <th scope="col" className="px-4 md:px-12 py-2 md:py-3">
                   Owner/Organization
                 </th>
-                <th scope="col" className="px-12 py-3">
+                <th scope="col" className="px-4 md:px-12 py-2 md:py-3">
+                  Description
+                </th>
+                <th scope="col" className="px-4 md:px-12 py-2 md:py-3">
                   Repos
                 </th>
-                <th scope="col" className="px-12 py-3">
+                <th scope="col" className="px-4 md:px-12 py-2 md:py-3">
                   Last update
                 </th>
               </tr>
             </thead>
             <tbody>
-              {owners.map(({ id, reposCount }) => (
+              {owners.map(({ id, reposCount, avatarUrl, description }) => (
                 <tr
                   key={id}
                   className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                 >
-                  <th
+                  <td className="px-4 md:px-12 py-4 md:py-8">
+                    <Image
+                      src={avatarUrl}
+                      width={64}
+                      height={64}
+                      alt={`${id} org avatar`}
+                    />
+                  </td>
+                  <td
                     scope="row"
-                    className="px-12 py-8 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    className="px-4 md:px-12 py-4 md:py-8 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
                     <Link
                       href={`/lens/${id}`}
@@ -48,9 +68,10 @@ export default async function Home() {
                     >
                       {id}
                     </Link>
-                  </th>
-                  <td className="px-12 py-8">{reposCount}</td>
-                  <td className="px-12 py-8">Not available</td>
+                  </td>
+                  <td className="px-4 md:px-12 py-4 md:py-8">{description}</td>
+                  <td className="px-4 md:px-12 py-4 md:py-8">{reposCount}</td>
+                  <td className="px-4 md:px-12 py-4 md:py-8">Not available</td>
                 </tr>
               ))}
             </tbody>
