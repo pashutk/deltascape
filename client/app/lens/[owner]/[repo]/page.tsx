e272760588db56a09fd24572a6e01a0ecd2727c0
pashutk <me@pashutk.ru>
2023-05-11 00:46:04 +0200

    Change project page layout

--- client/app/lens/[owner]/[repo]/page.tsx
+++ client/app/lens/[owner]/[repo]/page.tsx
@@ -1,5 +1,7 @@
 import { formatRelative, parseISO } from "date-fns";
 import { MongoClient } from "mongodb";
+import Link from "next/link";
+import styles from "./index.module.css";
 
 async function fetchPRs(owner: string, repo: string) {
   const supportedOrgs = [
@@ -48,9 +50,36 @@ export default async function Repo({ params: { owner, repo } }: Props) {
   return (
     <main className="min-h-screen items-center justify-between p-8 md:p-24">
       <div className="relative place-items-center">
-        <h1 className="text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white mb-8 md:mb-16">
+        <h1 className="text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white mb-8 md:mb-16 max-w-5xl mx-auto">
           {owner}/{repo} pull requests
         </h1>
+        {/* <div className="max-w-5xl mx-auto mb-8">
+          <p className="text-center text-gray-500 dark:text-gray-400">
+            This week, major changes include bug fixes, improvements in
+            stability and code structure, and the addition of new features like
+            an auto-clicker, weather station protocol support, and API version
+            in device info.{" "}
+            <Link
+              href="/"
+              className="inline-flex items-center font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            >
+              Here is a summary of the changes
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 ml-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </Link>
+          </p>
+        </div> */}
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg max-w-5xl mx-auto">
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -74,7 +103,7 @@ export default async function Repo({ params: { owner, repo } }: Props) {
                 >
                   <td
                     scope="row"
-                    className="px-4 md:px-8 lg:px-12 py-8 font-medium text-gray-900 dark:text-white"
+                    className="px-4 md:px-8 lg:px-12 py-2 font-medium text-gray-900 dark:text-white"
                   >
                     {title}{" "}
                     <span className="whitespace-nowrap">
@@ -88,10 +117,10 @@ export default async function Repo({ params: { owner, repo } }: Props) {
                       )
                     </span>
                   </td>
-                  <td className="px-4 md:px-8 lg:px-12 py-8">
+                  <td className="px-4 md:px-8 lg:px-12 py-2">
                     {formatRelative(mergedAt, new Date())}
                   </td>
-                  <td className="px-4 md:px-8 lg:px-12 py-8">{summary}</td>
+                  <td className="px-4 md:px-8 lg:px-12 py-2">{summary}</td>
                 </tr>
               ))}
             </tbody>
